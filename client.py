122695eab2a3e821f9a7582fbb09eedf030961d8
ben-likes-otters <66840761+ben-likes-otters@users.noreply.github.com>
2024-01-16 09:43:46 -0500

    Update client.py
    Created a client that fetches code from github and syncs the code on a local machine

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+import requests, os
+
+url = "https://raw.githubusercontent.com/ben-likes-otters/transcription/main/ev2prode.py"
+
+try:
+    code = requests.get(url).content.decode()
+
+    with open("updated.py","w") as f:
+        f.write(code)
+except:
+    print("NOT UPDATED")
+    
+os.system("python3 updated.py")
 
